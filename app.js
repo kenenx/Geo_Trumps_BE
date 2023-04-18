3a721c838684f98b0f721e7c769f7f2b5eaf70dd
kenenx <kenen1996@hotmail.co.uk>
2023-04-18 16:04:04 +0100

    add post/patch for player.json

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.json())
 app.use(logger);
 
 const countries = require('./countries.json')
+const players = require('./players.json')
 
 app.get('/', (req, res) => {
     res.send(`Welcome to the Geo Trumps API! There are ${countries.length} available.`);
@@ -35,5 +36,28 @@ app.get('/countries/:country', (req, res) => {
     res.send(country)
 })
 
+app.get('/players', (req, res) => {
+    res.send(players);
+})
+
+app.post("/players/:user", (req, res) => {
+    const player = players.find(
+        (player) => player.user.toLowerCase() === req.params.user.toLowerCase()
+    );
+    if (player === undefined) {
+        return res.status(404).send({ error: "Player does not exist" });
+    }
+    try {
+        const updatedPlayer = {...req.body, user: req.body.user, id: user.id,};
+        console.log("line 75", updatedPlayer);
+        const idx = players.findIndex((u) => u.id === user.id);
+        console.log(idx);
+        players[idx] = updatedPlayer;
+        console.log(players[idx]);
+        res.send(updatedPlayers);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
 
 module.exports = app;
